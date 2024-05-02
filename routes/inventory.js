ba09be2339ae872b1241fe83adfd94fb36cea6d8
DrantDumani <darnjames@gmail.com>
2024-05-02 12:52:48 -0400

    Write CRUD operations for category routes

--- routes/inventory.js
+++ routes/inventory.js
@@ -23,8 +23,6 @@ router.get("/item/:id", itemController.single_item);
 
 router.get("/categories", categoryController.category_list);
 
-router.get("/category/:id", categoryController.single_category);
-
 router.get("/category/create", categoryController.category_create_get);
 
 router.post("/category/create", categoryController.category_create_post);
@@ -36,5 +34,7 @@ router.post("/category/:id/delete", categoryController.category_delete_post);
 router.get("/category/:id/update", categoryController.category_update_get);
 
 router.post("/category/:id/update", categoryController.category_update_post);
+
+router.get("/category/:id", categoryController.single_category);
 
 module.exports = router;
