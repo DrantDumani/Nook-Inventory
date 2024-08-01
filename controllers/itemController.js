f9c021c09e2376648cfe43ee6ce6cadea3575671
Darnell <darnjames@gmail.com>
2024-08-01 04:34:30 -0400

    Finish migrating from mongo to postgres

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -19,14 +19,6 @@ cloudinary.config({
   api_secret: process.env.CLOUD_API_SECRET,
 });
 
-// exports.OLDindex = asyncHandler(async (req, res, next) => {
-//   const [itemCount, categoryCount] = await Promise.all([
-//     Item.countDocuments().exec(),
-//     Category.countDocuments().exec(),
-//   ]);
-//   res.render("index", { title: "Inventory", itemCount, categoryCount });
-// });
-
 exports.index = asyncHandler(async (req, res, next) => {
   const result = await queries.countItemsAndCategories();
   const { itemtotal, categorytotal } = result[0];
@@ -36,11 +28,6 @@ exports.index = asyncHandler(async (req, res, next) => {
     categoryCount: categorytotal,
   });
 });
-
-// exports.OLDitem_list = asyncHandler(async (req, res, next) => {
-//   const allItems = await Item.find({}, "name price").sort({ name: 1 }).exec();
-//   res.render("itemList", { allItems, title: "Item List" });
-// });
 
 exports.item_list = asyncHandler(async (req, res, next) => {
   const allItems = await queries.getItemList();
@@ -82,25 +69,12 @@ exports.single_item = asyncHandler(async (req, res, next) => {
   });
 });
 
-// exports.OLDitem_create_get = asyncHandler(async (req, res, next) => {
-//   const allCategories = await Category.find().sort({ name: 1 }).exec();
-
-//   res.render("itemForm", {
-//     title: "Create Item",
-//     allCategories,
-//   });
-// });
-
 exports.item_create_get = asyncHandler(async (req, res, next) => {
   const allCategories = await queries.getCategoryList();
 
   res.render("itemForm", { title: "Create Item", allCategories });
 });
 
-//create the item first
-//after the item is successfully created, upload the image to cloudinary and store its data in table
-// if the item does not get successfully created, then there is no point in even uploading the image to cloudinary
-//ALSO, the server will not be responsible for filtering duplicate names.
 exports.item_create_post = [
   upload.single("image-file"),
   (req, res, next) => {
@@ -121,12 +95,11 @@ exports.item_create_post = [
     .withMessage("Price must be whole number greater than 0"),
 
   asyncHandler(async (req, res, next) => {
-    console.log(req.file);
     const errors = validationResult(req).array();
     const item = {
       name: req.body.name,
       description: req.body.description,
-      inStock: req.body.stock,
+      instock: req.body.stock,
       price: req.body.price,
       category: req.body.category,
     };
@@ -135,7 +108,7 @@ exports.item_create_post = [
       const allCategories = await queries.getCategoryList();
 
       for (const category of allCategories) {
-        if (item.category.includes(category.id)) {
+        if (item.category.includes(category.id.toString())) {
           category.checked = "true";
         }
       }
@@ -171,93 +144,12 @@ exports.item_create_post = [
   }),
 ];
 
-// exports.OLDitem_create_post = [
-//   upload.single("image-file"),
-//   (req, res, next) => {
-//     if (!Array.isArray(req.body.category)) {
-//       req.body.category = req.body.category ? [req.body.category] : [];
-//     }
-//     next();
-//   },
-
-//   body("name", "Name must not be empty").trim().isLength({ min: 1 }),
-//   body("description", "Summary must not be blank").trim().isLength({ min: 1 }),
-//   body("category", "Pick at least one category").isArray({ min: 1 }),
-//   body("stock", "Stock cannot be empty")
-//     .isInt({ min: 0 })
-//     .withMessage("Stock must be whole number greater than -1"),
-//   body("price", "Price must not be empty")
-//     .isInt({ min: 1 })
-//     .withMessage("Price must be whole number greater than 0"),
-
-//   asyncHandler(async (req, res, next) => {
-//     const errors = validationResult(req).array();
-//     let duplicateNameErr = "";
-
-//     const nameExists = await Item.findOne({ name: req.body.name })
-//       .collation({ locale: "en", strength: 2 })
-//       .exec();
-
-//     if (nameExists) {
-//       duplicateNameErr =
-//         "There is another item with that name in the inventory.";
-//     }
-
-//     const item = new Item({
-//       name: req.body.name,
-//       description: req.body.description,
-//       category: req.body.category,
-//       inStock: req.body.stock,
-//       price: req.body.price,
-//     });
-
-//     if (errors.length || duplicateNameErr) {
-//       const allCategories = await Category.find().sort({ name: 1 }).exec();
-
-//       for (const category of allCategories) {
-//         if (item.category.includes(category._id)) {
-//           category.checked = "true";
-//         }
-//       }
-
-//       res.render("itemForm", {
-//         title: "Create item",
-//         allCategories,
-//         item: item,
-//         errors: errors,
-//         nameErr: duplicateNameErr,
-//       });
-//     } else {
-//       if (req.file) {
-//         const upload = await new Promise((resolve) => {
-//           cloudinary.uploader
-//             .upload_stream((err, result) => {
-//               return resolve(result);
-//             })
-//             .end(req.file.buffer);
-//         });
-//         const { public_id } = upload;
-//         const transformURL = cloudinary.url(public_id, {
-//           width: 128,
-//           height: 128,
-//         });
-//         item.image = { publicId: public_id, url: transformURL };
-//       }
-//       await item.save();
-//       res.redirect(item.url);
-//     }
-//   }),
-// ];
-
 exports.item_delete_get = asyncHandler(async (req, res, next) => {
-  const item = await Item.findById(req.params.id, "name inStock").exec();
+  const item = await queries.getItemNameAndStock(req.params.id);
 
   if (!item) res.redirect("/inventory/items");
 
-  res.render("itemDelete", {
-    title: "Delete Item",
-    singleItem: item,
-  });
+  res.render("itemDelete", { title: "Delete Item", singleItem: item });
 });
 
 exports.item_delete_post = [
@@ -267,9 +159,9 @@ exports.item_delete_post = [
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
-      const item = await Item.findById(req.params.id, "name inStock").exec();
+      const item = await queries.getItemNameAndStock(req.params.id);
 
-      if (!item) res.redirect("/inventory/items");
+      if (!item) return res.redirect("/inventory/items");
       else {
         res.render("itemDelete", {
           title: "Delete Item",
@@ -278,9 +170,9 @@ exports.item_delete_post = [
         });
       }
     } else {
-      const deleted = await Item.findByIdAndDelete(req.body.itemId).exec();
-      if (deleted.image.url) {
-        await cloudinary.uploader.destroy(deleted.image.publicId);
+      const deletedItem = await queries.deleteItemById(req.params.id);
+      if (deletedItem) {
+        await cloudinary.uploader.destroy(deletedItem.img_public_id);
       }
       res.redirect("/inventory/items");
     }
@@ -289,8 +181,8 @@ exports.item_delete_post = [
 
 exports.item_update_get = asyncHandler(async (req, res, next) => {
   const [item, allCategories] = await Promise.all([
-    Item.findById(req.params.id).exec(),
-    Category.find({}, "name").sort({ name: 1 }).exec(),
+    queries.getItemById(req.params.id),
+    queries.getCategoryList(),
   ]);
 
   if (!item) {
@@ -300,11 +192,13 @@ exports.item_update_get = asyncHandler(async (req, res, next) => {
   }
 
   allCategories.forEach((cat) => {
-    if (item.category.includes(cat._id)) cat.checked = "true";
+    if (item.categories.findIndex((catObj) => catObj.catId === cat.id) > -1) {
+      cat.checked = true;
+    }
   });
 
   res.render("itemForm", {
-    title: "Update Item",
+    title: "Update Form",
     item: item,
     allCategories: allCategories,
     requirePass: true,
@@ -333,39 +227,21 @@ exports.item_update_post = [
 
   asyncHandler(async (req, res, next) => {
     const errors = validationResult(req);
-    let duplicateNameErr = "";
 
-    const [validId, nameExists] = await Promise.all([
-      Item.findById(req.params.id, "name image").exec(),
-      Item.findOne({ name: req.body.name })
-        .collation({ locale: "en", strength: 2 })
-        .exec(),
-    ]);
-
-    if (
-      validId &&
-      nameExists &&
-      validId._id.toString() !== nameExists._id.toString()
-    ) {
-      duplicateNameErr =
-        "There is another item with that name in the inventory.";
-    }
-
-    const item = new Item({
+    const item = {
       name: req.body.name,
       description: req.body.description,
       category: req.body.category,
-      inStock: req.body.stock,
+      instock: req.body.stock,
       price: req.body.price,
-      image: { url: validId?.image?.url, publicId: validId?.image?.publicId },
-      _id: req.params.id,
-    });
+      id: req.params.id,
+    };
 
-    if (!errors.isEmpty() || duplicateNameErr) {
-      const allCategories = await Category.find().sort({ name: 1 }).exec();
+    if (!errors.isEmpty()) {
+      const allCategories = await queries.getCategoryList();
 
       for (const category of allCategories) {
-        if (item.category.includes(category._id)) {
+        if (item.category.includes(category.id.toString())) {
           category.checked = "true";
         }
       }
@@ -375,33 +251,38 @@ exports.item_update_post = [
         allCategories,
         item: item,
         errors: errors.array(),
-        nameErr: duplicateNameErr,
         requirePass: true,
       });
     } else {
+      const updatedItemIds = await queries.updateItem(item);
+      console.log(updatedItemIds);
       if (req.file) {
-        const { publicId } = validId.image;
-        let upload = null;
+        const publicId = updatedItemIds.img_public_id;
         const options = publicId
           ? { public_id: publicId, invalidate: true, overwrite: true }
           : {};
-        upload = await new Promise((resolve) => {
+        const upload = await new Promise((resolve) => {
           cloudinary.uploader
             .upload_stream(options, (err, result) => {
               return resolve(result);
             })
             .end(req.file.buffer);
         });
+
         const { public_id } = upload;
+        console.log(public_id);
         const transformURL = cloudinary.url(public_id, {
           width: 128,
           height: 128,
           version: upload.version,
         });
-        item.image = { publicId: public_id, url: transformURL };
+        await queries.insertOrUpdateImage(
+          updatedItemIds.id,
+          transformURL,
+          public_id
+        );
       }
-      const updatedItem = await Item.findByIdAndUpdate(req.params.id, item);
-      res.redirect(updatedItem.url);
+      return res.redirect(`/inventory/item/${updatedItemIds.id}`);
     }
   }),
 ];
--- db/queries.js
+++ db/queries.js
@@ -44,7 +44,7 @@ exports.createItem = async (item) => {
   const { rows } = await pool.query(insertItemStr, [
     item.name,
     item.description,
-    item.inStock,
+    item.instock,
     item.price,
     item.category,
   ]);
@@ -55,11 +55,68 @@ exports.insertOrUpdateImage = async (id, imgUrl, imgId) => {
   const { rows } = await pool.query(
     `INSERT INTO item_images (item_id, image, img_public_id) VALUES
     ($1, $2, $3) ON CONFLICT (item_id) DO UPDATE SET 
-    item_id = EXCLUDED.item_id,
     image = EXCLUDED.image,
     img_public_id = EXCLUDED.img_public_id`,
     [id, imgUrl, imgId]
   );
+};
+
+exports.getItemNameAndStock = async (id) => {
+  const { rows } = await pool.query(
+    "SELECT id, name, inStock FROM items WHERE id = $1",
+    [id]
+  );
+  return rows[0];
+};
+
+exports.deleteItemById = async (id) => {
+  const { rows } = await pool.query(
+    `WITH deleted_item AS (
+      DELETE FROM items WHERE id = $1 RETURNING id
+    )
+    DELETE FROM item_images WHERE item_id = (
+      SELECT id FROM deleted_item
+    ) RETURNING img_public_id;`,
+    [id]
+  );
+  return rows[0];
+};
+
+exports.updateItem = async (item) => {
+  const client = await pool.connect();
+
+  try {
+    await client.query("BEGIN");
+    const updateItemQuery = `UPDATE items SET name = $2, description = $3, inStock = $4, price = $5
+      WHERE id = $1`;
+    await client.query(updateItemQuery, [
+      item.id,
+      item.name,
+      item.description,
+      item.instock,
+      item.price,
+    ]);
+    const removeCatQuery = `DELETE FROM item_categories WHERE item_id = $1 AND cat_id <> ANY ($2)`;
+    await client.query(removeCatQuery, [item.id, item.category]);
+    const addNewCatQuery = `INSERT INTO item_categories (item_id, cat_id) 
+      SELECT $1, UNNEST($2::int[]) ON CONFLICT DO NOTHING`;
+    await client.query(addNewCatQuery, [item.id, item.category]);
+    const { rows } = await client.query(
+      `SELECT items.id, item_images.img_public_id FROM 
+       items LEFT JOIN item_images ON items.id = item_images.item_id
+       WHERE items.id = $1`,
+      [item.id]
+    );
+
+    await client.query("COMMIT");
+    return rows[0];
+  } catch (err) {
+    await client.query("ROLLBACK");
+    console.log(err);
+    return err;
+  } finally {
+    client.release();
+  }
 };
 
 exports.getCategoryList = async () => {
